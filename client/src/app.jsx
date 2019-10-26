a2795ebbfb7a24c536b7e9f3445e04f5a918a50c
Keenan Thomson <keenanthomson@gmail.com>
2019-10-26 17:10:22 -0400

    Update styling, make progress onChange functions

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-// import ReactDOM from 'react-dom';
+import axios from 'axios';
+import DatePicker from "react-datepicker";
+import './app.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      testID: null,
+      startDate: null,
+      endDate: null,
     };
   };
 
@@ -14,17 +19,46 @@ export default class App extends Component {
   }
 
   renderFile() {
+    // let requestBody = renderBody();
+    let requestBody = {
+      testID: this.state.testID,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+    }
+    axios
+    .post(`http://localhost:3001/api/renderfile`, requestBody)
+    .then(response => {
+      console.log(`Server Response: ${response.data}`);
+    });
+  };
+
+  handleTestIDChange(e) {
+    this.setState({testID: e.target.value});
+  };
+
+  handleStartDateChange(e) {
+    console.log(e);
+    //NEXT get ^^ into preferred format for SQL -> '2019-10-21'
+  }
+
+  handleEndDateChange(e) {
 
   }
 
   render() {
     return(
-      <div>
-        <div>SQL Script Generator</div>
-        <button onClick={() => this.renderFile}></button>
+      <div className="box">
+        <div className="header">SQL Script Generator</div>
+        <div className="entries">
+          <div className="input-name"><span>TestID</span></div>
+          <div className="input-name"><span>Start Date</span></div>
+          <div className="input-name"><span>End Date</span></div>
+          <input className="entry-input" name="testID" onChange={this.handleTestIDChange}></input>
+          <DatePicker dateFormat="yyyy-mm-dd" placeholderText="select start date" className="entry-input" name="startDate" selected={this.state.startDate} onChange={this.handleStartDateChange}/>
+          <DatePicker dateFormat="yyyy-mm-dd" placeholderText="select end date" className="entry-input" name="endDate" selected={this.state.endDate} onChange={this.handleEndDateChange}/>
+        </div>
+        <input className="submit" type="submit" onClick={() => this.renderFile()}></input>
       </div>
     )
   };
 };
-
-// ReactDOM.render(<App/>, document.getElementById('app'));
--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+// const fs = require('fs');
+
+// START HERE -> take in data from client -> write file with fs -> download file to client
 
 app.use(express.static('./client/dist'));
+
+app.post('/api/renderfile', async (req, res) => {
+
+  // await fs.writeFile(temp.sql, req.body, (err) => {
+  //   if (err) {
+  //     console.log(`writeFile error: ${err}`);
+  //   };
+  // });
+  console.log(req.body);
+  res.send("Server here!");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
