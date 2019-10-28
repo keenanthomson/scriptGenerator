1f19836f1b9dc9a1c4d1882cc0994bae3a8a54be
Keenan Thomson <keenanthomson@gmail.com>
2019-10-28 00:05:49 -0400

    Work on formatting dates

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -12,6 +12,7 @@ export default class App extends Component {
       startDate: null,
       endDate: null,
     };
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
   };
 
   componentDidMount() {
@@ -36,9 +37,23 @@ export default class App extends Component {
     this.setState({testID: e.target.value});
   };
 
-  handleStartDateChange(e) {
-    console.log(e);
+  handleStartDateChange(date) {
+    console.log(this.formatDate(date))
+    
     //NEXT get ^^ into preferred format for SQL -> '2019-10-21'
+
+    // this.setState({startDate: e});
+  }
+
+  formatDate(date) {
+    let splitData = new Intl.DateTimeFormat().format(date).split("/");
+    let formattedDate;
+    console.log(splitData[2])
+    for (let i = splitData.length - 1; i < 0; i--) {
+      console.log(splitData[i]) // not working as I expect, undefined instead of '2019'
+      formattedDate = formattedDate + `${splitData[i]}-`;
+    }
+    return formattedDate;
   }
 
   handleEndDateChange(e) {
@@ -54,7 +69,7 @@ export default class App extends Component {
           <div className="input-name"><span>Start Date</span></div>
           <div className="input-name"><span>End Date</span></div>
           <input className="entry-input" name="testID" onChange={this.handleTestIDChange}></input>
-          <DatePicker dateFormat="yyyy-mm-dd" placeholderText="select start date" className="entry-input" name="startDate" selected={this.state.startDate} onChange={this.handleStartDateChange}/>
+          <DatePicker dateFormat="yyyy-mm-dd" placeholderText="select start date" className="entry-input" name="startDate" maxDate={new Date()} selected={this.state.startDate} onChange={this.handleStartDateChange}/>
           <DatePicker dateFormat="yyyy-mm-dd" placeholderText="select end date" className="entry-input" name="endDate" selected={this.state.endDate} onChange={this.handleEndDateChange}/>
         </div>
         <input className="submit" type="submit" onClick={() => this.renderFile()}></input>
